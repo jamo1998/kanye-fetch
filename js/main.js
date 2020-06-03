be0355d086971a0ac74601742e0510a5c0e72237
Jameson Aardalen <jamesonaardalen@Jamesons-MBP.attlocal.net>
2020-06-03 16:05:59 -0700

    added dad quote button

--- js/main.js
+++ js/main.js
@@ -1,1 +1,43 @@
-console.log("👀")
+let url = "https://api.kanye.rest/";
+let dadUrl = "https://icanhazdadjoke.com/";
+let quote = document.getElementById("quote");
+let dadQuoteBtn = document.getElementById("dad-quote");
+let kanyeQuoteBtn = document.getElementById("new-quote");
+
+// -------- EVENT LISTENERS -----------
+kanyeQuoteBtn.addEventListener("click", getKanyeQuote);
+dadQuoteBtn.addEventListener("click", getDadQuote);
+
+function getKanyeQuote() {
+  fetch(url)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      // console.log(data);
+      // clear current quote before requesting new one
+      quote.innerText = "";
+      // display incoming quote in the DOM
+      quote.textContent = `"${data.quote}" - Kanye West`;
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+
+function getDadQuote() {
+  fetch(url)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      // console.log(data);
+      // clear current quote before requesting new one
+      quote.innerText = "";
+      // display incoming quote in the DOM
+      quote.innerText = `"${data.quote}" - Random Ass Dad`;
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
